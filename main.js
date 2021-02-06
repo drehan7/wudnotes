db7b0bc3aae73d07f93ec223a891bdcfcffad8ab
Dawud Rehan <d.rehan7@gmail.com>
2021-02-06 18:40:29 -0500

    all requests working. need to finish styling and then we gucci

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let form = document.querySelector('form');
 
 showAllNotes();
 
+// ---------------------------------------------------------------
 
 window.addEventListener('submit', e => {
     e.preventDefault();
@@ -25,30 +26,30 @@ document.addEventListener('click', e => {
         console.log("del button clicked")
         delNote(e.target);
     } else if (e.target.className === 'edit-button') {
-        console.log("Edit button clicked", e.target.parentElement.id)
+        // console.log("Edit button clicked", e.target.parentElement.id)
         toggleEditMode(e.target)
     } else if (e.target.className === 'edit-submit-button') {
         console.log("edit submit clicked");
+        editNote(e.target)
     }
 })
 
 
 
 
-
-// --------------------------------
+// ---------------------------------------------------------------
 function postNote() {
-    let noteTitle = document.querySelector(".new-note-title").value;
-    let noteBody = document.querySelector(".new-note-input").value;
+    let noteTitle = document.querySelector(".new-note-title");
+    let noteBody = document.querySelector(".new-note-input");
     let dateCreated = new Date();
 
-    if (noteTitle.length > 1 || noteBody.length > 1) {
+    if (noteTitle.value.length > 1 || noteBody.value.length > 1) {
         fetch (baseNotesURL, {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
-                title: noteTitle,
-                body: noteBody,
+                title: noteTitle.value,
+                body: noteBody.value,
                 date_created: moment(dateCreated).format('llll')
             })})
             .then(res => res.json())
@@ -58,20 +59,50 @@ function postNote() {
         }
 
     noteTitle.value = "";
-    noteBody.value = '';
+    noteBody.value = "";
+
+    
 }
 
+
 function toggleEditMode(note) {
+    let title = note.parentElement.querySelector(".note-title").textContent
+    let body = note.parentElement.querySelector(".note-body").textContent
+
+
     let editInputblock = note.parentElement.querySelector('.edit-container');
     if (editInputblock.style.display === 'none') {
         editInputblock.style.display = 'block'
+        editInputblock.querySelector(".edit-input-title").value = title;
+        editInputblock.querySelector(".edit-input-body").value = body;
     } else {
         editInputblock.style.display = 'none'
     }
 
 }
 
+// TODO ---------------------------------------------------------------
 function editNote (note) {
+    const noteId = note.parentElement.parentElement.id;
+    console.log('url: ', baseNotesURL + noteId)
+    let editTitle = note.parentElement.querySelector(".edit-input-title");
+    let editBody = note.parentElement.querySelector(".edit-input-body");
+    let date_updated = new Date();
+
+    if (editTitle.value.length > 0 || editBody.value.length > 0) {
+        fetch (baseNotesURL + noteId, { 
+            method: 'PATCH',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                title: editTitle.value,
+                body: editBody.value,
+                date_created: moment(date_updated).format("llll")
+            })})
+            .then(res => res.json())
+            .then(data => console.log(data))
+        }
+    showAllNotes()
+
 
 }
 
@@ -89,6 +120,8 @@ function delNote(note) {
 
 
 function showAllNotes() {
+    let notesContainer = document.querySelector(".all-notes");
+    notesContainer.innerHTML = "";
     fetch (baseNotesURL)
         .then(res => res.json())
         .then(data => {
@@ -124,13 +157,18 @@ function renderNote(note) {
     editButton.innerHTML = "Edit";
     let editDiv = document.createElement('div');
     editDiv.className = 'edit-container';
-    let editInput = document.createElement('input');
-    editInput.className = 'edit-input';
+    let editInputTitle = document.createElement('input');
+    editInputTitle.className = 'edit-input-title';
+    editInputTitle.placeholder = "Title:"
+    let editInputBody = document.createElement('input');
+    editInputBody.className = 'edit-input-body';
+    editInputBody.placeholder = "Note:"
     let editSubmit = document.createElement('button');
     editSubmit.className = 'edit-submit-button';
     editSubmit.innerHTML = "Edit Note";
 
-    editDiv.appendChild(editInput);
+    editDiv.appendChild(editInputTitle);
+    editDiv.appendChild(editInputBody);
     editDiv.appendChild(editSubmit);
     editDiv.style.display = 'none';
 
